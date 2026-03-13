f3c2ca595f065ac84c8311e25a4010de95e0b164
Abdelhamid <elhaddadi.abdo.2003@gmail.com>
2026-03-13 06:26:40 +0000

    fix db config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`\n🚀 [SERVER] GearGrid Pro API running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`\n🚀 [SERVER] GearGrid Pro API running on port ${PORT}`);
   console.log(`📦 [DB]     Connected to MariaDB — ${process.env.DB_NAME}\n`);
 });
